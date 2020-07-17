be03e9a7dde877f7aca4a0415c71162f8a6c84da
toritheterrible <torimpham@gmail.com>
2020-07-17 15:25:17 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Home from "./pages/Home";
+import Container from "@material-ui/core/Container"
 
 function App() {
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container maxWidth="lg">
+        <Home/>
+      </Container>
     </div>
   );
 }
+
 
 export default App;
