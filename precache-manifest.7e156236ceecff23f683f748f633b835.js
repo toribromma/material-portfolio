9e9b9512faa1b5389f4409b181ca155e9558731a
Tori <torimpham@gmail.com>
2020-07-22 18:17:09 -0700

    Updates

---
+++ precache-manifest.7e156236ceecff23f683f748f633b835.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2777f964859b10b5918d9f3d31903ac4",
+    "url": "/material-portfolio/index.html"
+  },
+  {
+    "revision": "f8abdd623d391ea2e28a",
+    "url": "/material-portfolio/static/css/main.5a42a4c7.chunk.css"
+  },
+  {
+    "revision": "c8d655ec40f96c6ca1aa",
+    "url": "/material-portfolio/static/js/2.4943a668.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/material-portfolio/static/js/2.4943a668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8abdd623d391ea2e28a",
+    "url": "/material-portfolio/static/js/main.ad13181b.chunk.js"
+  },
+  {
+    "revision": "5e84320e9a2ad7ed8650",
+    "url": "/material-portfolio/static/js/runtime-main.cd17476b.js"
+  },
+  {
+    "revision": "7db8058a545c81c3f73f5c40e5f72721",
+    "url": "/material-portfolio/static/media/airbnb3.7db8058a.png"
+  },
+  {
+    "revision": "60af899a97f94a7b60c0a1f063513131",
+    "url": "/material-portfolio/static/media/profile1.60af899a.jpg"
+  },
+  {
+    "revision": "071439f20a59bde22f0420617cc00d06",
+    "url": "/material-portfolio/static/media/societe.071439f2.png"
+  },
+  {
+    "revision": "b61e0bf9552076cb34553c83e4e0a610",
+    "url": "/material-portfolio/static/media/wmp.b61e0bf9.png"
+  }
+]);
