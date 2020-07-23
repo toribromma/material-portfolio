d2c6d698b51fcf7a654741669c6da6ef0ca97980
Tori <torimpham@gmail.com>
2020-07-22 20:40:37 -0700

    changed basename

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
       <Router 
       forceRefresh={true}
-      basename="/material-portfolio">
+      basename={process.env.PUBLIC_URL}>
         <Container maxWidth="lg">
           <Header/>
           <Box component="span">
